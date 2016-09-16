dbd59a93b45fe9288fcb8714431f073e85756af3
danelumax <474198699@qq.com>
2016-09-16 22:14:32 +0800

    NDBTest add ResultSet

---
+++ C++/mysql/NDBTest/include/ResultSet.h
@@ -0,0 +1,18 @@
+/*
+ * ResultSet.h
+ *
+ *  Created on: Sep 16, 2016
+ *      Author: eliwech
+ */
+
+#ifndef RESULTSET_H_
+#define RESULTSET_H_
+
+class ResultSet
+{
+public:
+	ResultSet();
+	virtual ~ResultSet();
+};
+
+#endif /* RESULTSET_H_ */
---
+++ C++/mysql/NDBTest/src/ResultSet.cpp
@@ -0,0 +1,17 @@
+/*
+ * ResultSet.cpp
+ *
+ *  Created on: Sep 16, 2016
+ *      Author: eliwech
+ */
+
+#include "ResultSet.h"
+
+ResultSet::ResultSet() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ResultSet::~ResultSet() {
+	// TODO Auto-generated destructor stub
+}
