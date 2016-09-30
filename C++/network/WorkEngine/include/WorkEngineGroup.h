a3d0f4805bf7715c603e5a9bd1926ee2489ca30f
danelumax <474198699@qq.com>
2016-09-30 22:39:47 +0800

    WorkEngineGroup empty class

---
+++ C++/network/WorkEngine/include/WorkEngineGroup.h
@@ -0,0 +1,18 @@
+/*
+ * WorkEngineGroup.h
+ *
+ *  Created on: Sep 30, 2016
+ *      Author: eliwech
+ */
+
+#ifndef WORKENGINEGROUP_H_
+#define WORKENGINEGROUP_H_
+
+class WorkEngineGroup
+{
+public:
+	WorkEngineGroup();
+	virtual ~WorkEngineGroup();
+};
+
+#endif /* WORKENGINEGROUP_H_ */
---
+++ C++/network/WorkEngine/src/WorkEngineGroup.cpp
@@ -0,0 +1,16 @@
+/*
+ * WorkEngineGroup.cpp
+ *
+ *  Created on: Sep 30, 2016
+ *      Author: eliwech
+ */
+
+#include "WorkEngineGroup.h"
+
+WorkEngineGroup::WorkEngineGroup()
+{
+}
+
+WorkEngineGroup::~WorkEngineGroup()
+{
+}
