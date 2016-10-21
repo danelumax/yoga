e0fb0b24047157def02ce18088c024cb1eb1264c
danelumax <474198699@qq.com>
2016-10-22 00:16:52 +0800

    Prototype Pattern All Done

--- C++/DesignPattern/Prototype/PrototypeManager/src/main.cpp
+++ C++/DesignPattern/Prototype/PrototypeManager/src/main.cpp
@@ -12,34 +12,50 @@
 
 int main()
 {
+	/* Initial Prototype Manager */
+	Prototype* prototype = NULL;
 	Prototype* p1 = new ConcretePrototype1();
 	PrototypeManager* manager = PrototypeManager::getInstance();
 	manager->setPrototype("Prototype1", p1);
-	Prototype* p3 = manager->getPrototype("Prototype1")->clone();
-	if (p3 != NULL)
+
+	/* Fetch the prototype */
+	prototype = manager->getPrototype("Prototype1");
+	if (prototype != NULL)
 	{
+		Prototype* p3 = prototype->clone();
 		p3->setName("John");
 		p3->toString();
 	}
 
+	/* Change the prototype */
 	Prototype* p2 = new ConcretePrototype2();
 	manager->setPrototype("Prototype1", p2);
 
-	Prototype* p4 = manager->getPrototype("Prototype1")->clone();
-	if (p4 != NULL)
+	/* Fetch the prototype */
+	prototype = manager->getPrototype("Prototype1");
+	if (prototype != NULL)
 	{
+		Prototype* p4 = prototype->clone();
 		p4->setName("Peter");
 		p4->toString();
 	}
 
+	/* Delete the prototype */
 	manager->removePrototype("Prototype1");
 
-	Prototype* p5 = manager->getPrototype("Prototype1")->clone();
-	if (p5 != NULL)
+	/* Fetch the prototype again*/
+	prototype = manager->getPrototype("Prototype1");
+	if (prototype != NULL)
 	{
+		Prototype* p5 = prototype->clone();
 		p5->setName("Tom");
 		p5->toString();
 	}
+
+	PrototypeManager::destory();
+	delete p1;
+	delete p2;
+	delete prototype;
 
 	return 0;
 }
