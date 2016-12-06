56edf495b159baa44047b353b3c375126613aafa
danelumax <474198699@qq.com>
2016-12-06 22:57:09 +0800

    Java Spring MVC : Form Test

---
+++ Java/Spring/MVC/FormTest/src/com/form/Student.java
@@ -0,0 +1,31 @@
+package com.form;
+public class Student {
+	private Integer age;
+	private String name;
+	private Integer id;
+   
+	public Student() {
+		this.name = new Welcome().getName();
+		this.age = 0;
+		this.id = 0;
+	}
+   
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getId() {
+		return id;
+	}
+}
---
+++ Java/Spring/MVC/FormTest/src/com/form/Welcome.java
@@ -0,0 +1,13 @@
+package com.form;
+
+public class Welcome {
+	private String name;
+	
+	public Welcome() {
+		this.name = "Welcome to Liwei Web";
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+}
