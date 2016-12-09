661dd0f3c3e8106220ab6997cf600f326642147b
danelumax <474198699@qq.com>
2016-12-09 09:44:39 +0800

    daily commit

--- Java/basic/Mysql/ExecuteSQL/src/com/mysql/MainApp.java
+++ Java/basic/Mysql/ExecuteSQL/src/com/mysql/MainApp.java
@@ -3,12 +3,13 @@ package com.mysql;
 public class MainApp {
 
 	public static void main(String[] args) throws Exception {
-		final String dropSQL = "drop table if exists my_test";
-		final String createSQL = "create table my_test ( " +
-				  				 "test_id int auto_increment primary key, " +
-				  				 "test_name varchar(255))";
-		final String insertSQL = "insert into my_test(test_name) values('Liwei')";
-		final String selectSQL = "select * from my_test";
+		final String dropSQL = "drop table if exists student_table";
+		final String createSQL = "create table student_table ( " +
+				  				 "student_id int auto_increment primary key, " +
+				  				 "student_name varchar(255)," +
+				  				 "student_age int)";
+		final String insertSQL = "insert into student_table(student_name) values('Liwei')";
+		final String selectSQL = "select * from student_table";
 		
 		ExecuteSQL es = new ExecuteSQL();
 		
