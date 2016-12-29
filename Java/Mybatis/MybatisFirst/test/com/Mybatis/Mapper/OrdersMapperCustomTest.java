fe59d7ec6436e169e218d2b24b61fb59d584b7e9
danelumax <474198699@qq.com>
2016-12-29 23:06:21 +0800

    Java Mybatis : Second Level Cache

--- Java/Mybatis/MybatisFirst/test/com/Mybatis/Mapper/OrdersMapperCustomTest.java
+++ Java/Mybatis/MybatisFirst/test/com/Mybatis/Mapper/OrdersMapperCustomTest.java
@@ -12,6 +12,7 @@ import org.junit.Test;
 
 import com.pojo.Orders;
 import com.pojo.OrdersCustom;
+import com.pojo.User;
 
 public class OrdersMapperCustomTest {
 
@@ -61,6 +62,36 @@ public class OrdersMapperCustomTest {
 		System.out.println(list);
 
 		sqlSession.close();
+	}
+	
+	@Test
+	public void testCache2() throws Exception {
+		SqlSession sqlSession1 = sqlSessionFactory.openSession();
+		SqlSession sqlSession2 = sqlSessionFactory.openSession();
+		SqlSession sqlSession3 = sqlSessionFactory.openSession();
+		
+		/* need sql */
+		UserMapper userMapper1 = sqlSession1.getMapper(UserMapper.class);
+		User user1 = userMapper1.findUserById(1);
+		System.out.println(user1);
+		
+		//这里执行关闭操作，将sqlsession中的数据写到二级缓存区域
+		sqlSession1.close();
+		
+//		/* clean cache */
+//		UserMapper userMapper3 = sqlSession3.getMapper(UserMapper.class);
+//		User user3 = userMapper3.findUserById(1);
+//		user3.setUsername("Liwei");
+//		userMapper3.updateUser(user3);
+//		//执行提交，清空UserMapper下边的二级缓存
+//		sqlSession3.commit();
+//		sqlSession3.close();
+		
+		UserMapper userMapper2 = sqlSession2.getMapper(UserMapper.class);
+		User user2 = userMapper2.findUserById(1);
+		System.out.println(user2);
+		
+		sqlSession1.close();
 	}
 
 }
